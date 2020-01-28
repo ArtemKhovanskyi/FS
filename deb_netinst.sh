90807cf56dc4bf8c6331b9bbb0eb70c83a29c2af
raketa888 <59506055+raketa888@users.noreply.github.com>
2020-01-28 04:03:56 +0200

    Update deb_netinst.sh

--- deb_netinst.sh
+++ deb_netinst.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 LIST_OF_APPS="htop nmap net-tools tcpdump iftop"
 
-aptitude update
-aptitude install -y $LIST_OF_APPS
+apt update
+apt install -y $LIST_OF_APPS
+apt upgrade
+
+shutdown -r +6
